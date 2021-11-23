6561222bf3c0a358f91e3b5805bc0d7a6f8ebe66
qhgogogo <qhgogogo@gmail.com>
2021-11-23 13:35:59 -0800

    Adds lateral subsurface flow

--- components/elm/src/biogeophys/SoilHydrologyMod.F90
+++ components/elm/src/biogeophys/SoilHydrologyMod.F90
@@ -927,7 +927,7 @@ contains
              !--  compute possible perched water table *and* groundwater table afterwards
              ! locate perched water table from bottom up starting at frost table
              ! sat_lev is an arbitrary saturation level used to determine perched water table
-             sat_lev=0.9
+             sat_lev=0.99
 
              k_perch=1
              do k=k_frz,1,-1
@@ -1276,7 +1276,7 @@ contains
              !--  compute possible perched water table *and* groundwater table afterwards
              ! locate perched water table from bottom up starting at frost table
              ! sat_lev is an arbitrary saturation level used to determine perched water table
-             sat_lev=0.9
+             sat_lev=0.99
 
              k_perch=1
              do k=k_frz,1,-1
@@ -1909,7 +1909,7 @@ contains
              !--  compute possible perched water table *and* groundwater table afterwards
              ! locate perched water table from bottom up starting at frost table
              ! sat_lev is an arbitrary saturation level used to determine perched water table
-             sat_lev=0.9
+             sat_lev=0.99
 
              k_perch=1
              do k=k_frz,1,-1
